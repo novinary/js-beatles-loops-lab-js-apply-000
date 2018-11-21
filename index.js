7b595cc6df874444b51ade376f568b126d12fac7
captivating-computer-3358 <novinary@hotmail.com>
2018-11-21 12:37:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlePlay(musicians, instruments)
   
   for (let i = 0, l = musicians.Length; i < l; i++)
   {
-    arr.push('${musicians[i]} plays ${instruments[i]}')
+    arr.push(${musicians[i]} plays ${instruments[i]}')
     {
     return arr
     }
