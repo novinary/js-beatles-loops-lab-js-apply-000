36b8de2e2deee2beaf053b1d59ef100eb42f2597
captivating-computer-3358 <novinary@hotmail.com>
2018-11-21 13:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function johnLennonFacts(facts)
   }
   
   -----------------------------------------------------------
-  function iLoveTheBeatles(Number)
+  function iLoveTheBeatles(n)
   {
     const array = []
     
